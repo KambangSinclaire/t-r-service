a628b2de92a171cd62af99ed106c39ee4007d404
kambangSinclaire <kambangsinclaire@gmail.com>
2023-08-03 02:11:38 +0100

    clean-up

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,5 @@
 use actix_cors::Cors;
 use actix_web::{http::header, web, App, HttpResponse, HttpServer, Responder};
-use async_trait::async_trait;
-use reqwest::Client as HttpClient;
 use serde::{Deserialize, Serialize};
 
 use std::collections::HashMap;
